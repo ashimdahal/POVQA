580912ac5f5c3a6641fdb6ebaaa026946fc0e6c2
Ashim Dahal <codeashim@gmail.com>
2025-04-16 10:44:59 -0500

    feat: code for headInfer

---
+++ scripts/long_context_lm/run_llama.py
@@ -0,0 +1,25 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from headinfer.cache import OffloadedCache
+from headinfer.mp import mp_headinfer, mp_simulate_decode
+
+model_name = "Qwen/Qwen2.5-3B"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+# Wrap the model with HeadInfer
+# headinfer_model = HeadInferModel(model)
+
+# Generate text with long context
+input_text = "Once upon a time in a galaxy far, far away..."
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+
+with torch.inference_mode():
+
+    # patch the model
+    mp_headinfer(model)
+    past_key_values = OffloadedCache()
+
+    model(input_ids=input_ids, past_key_values=past_key_values, use_cache=True, num_logits_to_keep=1)
