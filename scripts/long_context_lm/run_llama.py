03e3a87ff8f6068682a07593d3c9153b63f2a6a4
Ashim Dahal <codeashim@gmail.com>
2025-04-16 11:04:05 -0500

    feat: the actual real implementation

--- scripts/long_context_lm/run_llama.py
+++ scripts/long_context_lm/run_llama.py
@@ -4,16 +4,16 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from headinfer.cache import OffloadedCache
 from headinfer.mp import mp_headinfer, mp_simulate_decode
 
-model_name = "Qwen/Qwen2.5-3B"
+model_name = "meta-llama/Llama-3.2-1B"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, low_cpu_mem_usage=True, attn_implementation="flash_attention_2").to("cuda")
 
 # Wrap the model with HeadInfer
 # headinfer_model = HeadInferModel(model)
 
 # Generate text with long context
 input_text = "Once upon a time in a galaxy far, far away..."
-input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
 
 
 with torch.inference_mode():
